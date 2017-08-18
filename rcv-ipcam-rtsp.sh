3896395c936144ef0f89c1920b300c3b40221035
Francesco Oliva <oli_fra@libero.it>
2017-08-18 21:02:46 +0200

    add comment rtsp command

--- rcv-ipcam-rtsp.sh
+++ rcv-ipcam-rtsp.sh
@@ -1,4 +1,4 @@
-gst-launch-1.0 rtspsrc 
-location=rtsp://<cam_ip_address>  !  decodebin ! 
-autovideosink
+#receive ipstream from ip-cam rtsp protocol
 
+gst-launch-1.0 rtspsrc location=rtsp://<cam_ip_address>:<port>  !  decodebin ! autovideosink
+#gst-launch-1.0 -t -v -c rtspsrc location=rtsp://<cam_ip_adrr>:<port> !  decodebin ! videoconvert ! videoscale !  ximagesink
--- rcv-tcpclient1.sh
+++ rcv-tcpclient1.sh
@@ -1,2 +1,6 @@
+# receive stream with tcp from tcp server
+# sender pipeline example with raspberry pi:
+# raspivid -t 0 -h 720 -w 1080 -fps 25 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  \
+#  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=IP port=PORT 
 gst-launch-1.0 tcpclientsrc host=IP port=PORT !  gdpdepay ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! xvimagesink -e
 

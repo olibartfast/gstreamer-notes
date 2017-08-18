9e660180e5779d9ed88c4b5f90c3d2ba11d6da30
Francesco Oliva <oli_fra@libero.it>
2017-08-18 21:27:18 +0200

    stream from cam + h264 enc

---
+++ stream_cam_and_h264enc.sh
@@ -0,0 +1,10 @@
+#stream from pc cam with h264 encoding 
+gst-launch-1.0 -v v4l2src device=/dev/video0  ! videoscale ! videoconvert ! \ 
+videorate ! video/x-raw,framerate=30/1  ! \ 
+x264enc tune=zerolatency ! h264parse ! mpegtsmux ! tcpserversink host=IP port=PORT
+
+
+
+#gst-launch-1.0 -v v4l2src device=/dev/video0  ! videoscale ! videoconvert ! videorate ! \
+# video/x-raw,framerate=30/1  ! x264enc ! video/x-h264,profile=high ! h264parse ! mpegtsmux ! \
+# tcpserversink host=IP port=PORT

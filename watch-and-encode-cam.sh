34d8f54ca903ed4c9f5dc06cfbe739819b5dd36d
Francesco Oliva <oli_fra@libero.it>
2017-08-18 23:55:27 +0200

    watch and encode cam

---
+++ watch-and-encode-cam.sh
@@ -0,0 +1,11 @@
+#gst-launch-1.0 -v v4l2src device=/dev/video0 ! video/x-raw,format=YV12,width=1280,height=720 \
+#! tee name=t t. ! queue ! xvimagesink sync=false t. ! queue ! videoconvert ! videoscale !\
+#x264enc tune=zerolatency ! h264parse ! mp4mux ! \
+#filesink location=video.mp4 sync=false
+
+
+gst-launch-1.0 -v -t -c v4l2src device=/dev/video0 \
+    ! video/x-raw,format=YV12,width=1280,height=720 \
+    ! tee name=t t. ! queue ! xvimagesink sync=false t. ! queue ! \
+    videoconvert ! x264enc tune=zerolatency ! h264parse ! \
+    matroskamux ! filesink location='video.mkv' sync=false

2f7a7747d5be0701b9d877902b6e435fd2d9a386
Francesco Oliva <olibartfast@gmail.com>
2018-09-08 17:57:44 +0200

    Update stream-cam-and-h264enc.sh

--- stream-cam-and-h264enc.sh
+++ stream-cam-and-h264enc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#stream from pc cam with h264 encoding 
+#stream from pc(linux) cam with h264 encoding 
 gst-launch-1.0 -v v4l2src device=/dev/video0  ! videoscale ! videoconvert ! \ 
 videorate ! video/x-raw,framerate=30/1  ! \ 
 x264enc tune=zerolatency ! h264parse ! mpegtsmux ! tcpserversink host=IP port=PORT
@@ -10,3 +10,6 @@ x264enc tune=zerolatency ! h264parse ! mpegtsmux ! tcpserversink host=IP port=PO
 #gst-launch-1.0 -v v4l2src device=/dev/video0  ! videoscale ! videoconvert ! videorate ! \
 # video/x-raw,framerate=30/1  ! x264enc ! video/x-h264,profile=high ! h264parse ! mpegtsmux ! \
 # tcpserversink host=IP port=PORT
+
+  
+on windows use "ksvideosrc device-index=0" in place of "v4l2src device=/dev/video0"  

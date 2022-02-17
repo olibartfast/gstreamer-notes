3f7dd47d3623ffb3a8fdb9c4c5353263d9e084cf
Francesco Oliva <olibartfast@gmail.com>
2022-02-17 09:51:57 +0100

    Update watch-cam.sh

--- watch-cam.sh
+++ watch-cam.sh
@@ -1,1 +1,3 @@
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! tee name=t t. ! queue ! xvimagesink sync=false
+#!/bin/bash
+#bash watch-cam.sh <device video> (i.e. /dev/video0)
+gst-launch-1.0 -v v4l2src device=$1 ! tee name=t t. ! queue ! xvimagesink sync=false
